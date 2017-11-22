380f261bfb4f75e9de1736bb53f393b406877254
Simon Gabriel <simon.gabriel@heidelpay.de>
2017-11-22 14:30:19 +0100

    [feature] Invoice B2C Secured: Added UnitTests and refactored base class.

--- tests/Helper/BasePaymentMethodTest.php
+++ tests/Helper/BasePaymentMethodTest.php
@@ -20,6 +20,11 @@ use PHPUnit\Framework\Constraint\Constraint;
  */
 class BasePaymentMethodTest extends Unit
 {
+    const REFERENCE_ID = 'http://www.heidelpay.de';
+    const PAYMENT_FRAME_ORIGIN = self::REFERENCE_ID;
+    const CSS_PATH = self::REFERENCE_ID;
+    const TEST_AMOUNT = 23.12;
+
     /**
      * Authentication data for heidelpay api
      *
