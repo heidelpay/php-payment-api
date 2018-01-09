24b7f14ba0295a0cfd8d72f8ed7c06a590317188
Stephano Vogel <stephano.vogel@outlook.com>
2018-01-09 11:19:59 +0100

    Changed script and travis config in order to make use of travis_wait for PHP 5.6

--- tests/script/travis_install.sh
+++ tests/script/travis_install.sh
@@ -7,23 +7,10 @@ PHPVER=$(phpenv version-name)
 INSTALL_COMMAND="composer install --no-interaction --prefer-dist"
 UPDATE_COMMAND="composer update --no-interaction --prefer-source"
 
-echo "PHPVER: ${PHPVER}"
-
 if [ "$deps" == "no" ]; then
-
-    # if PHP version is 5, use travis_wait for up to 20 minutes waiting time.
-    if [[ "$PHPVER" =~ ^5\.[0-9]+$ ]]; then
-        echo "Executing ${INSTALL_COMMAND} with travis_wait..."
-        travis_wait ${INSTALL_COMMAND}
-    fi
-
-    if [[ "$PHPVER" =~ ^7\.[0-9]+$ ]]; then
-        echo "Executing ${INSTALL_COMMAND} ..."
-        ${INSTALL_COMMAND}
-    fi
+    ${INSTALL_COMMAND}
 fi
 
 if [ "$deps" == "high" ]; then
-    echo "Executing ${UPDATE_COMMAND} ..."
     ${UPDATE_COMMAND}
 fi
