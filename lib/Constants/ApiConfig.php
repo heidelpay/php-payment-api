723436defddbf55b758e013215ce5017523ac758
Simon Gabriel <simon_gabriel@gmx.de>
2018-01-31 15:15:14 +0100

    [feature] Updated documentation and version. (PHPLIB-25)
    Signed-off-by: Simon Gabriel <simon_gabriel@gmx.de>

--- lib/Constants/ApiConfig.php
+++ lib/Constants/ApiConfig.php
@@ -18,7 +18,7 @@ namespace Heidelpay\PhpPaymentApi\Constants;
  */
 class ApiConfig
 {
-    const SDK_VERSION = 'v1.3.0';
+    const SDK_VERSION = 'v1.4.0';
 
     const LIVE_URL = 'https://heidelpay.hpcgw.net/ngw/post';
     const TEST_URL = 'https://test-heidelpay.hpcgw.net/ngw/post';
