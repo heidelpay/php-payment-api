604b28bc61375f52ccfaeeaaceb79c92bf87f0e4
sixer1182 <simon.gabriel@gmx.de>
2020-06-24 16:02:24 +0200

    [change] (PHPLIB-283) Update composer call due to timeouts during install/update command.
    - add diagnose
    - raise verbosity

--- tests/script/travis_install.sh
+++ tests/script/travis_install.sh
@@ -4,10 +4,8 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 PHPVER=$(phpenv version-name)
-INSTALL_COMMAND="composer install --no-interaction --prefer-dist --verbose"
-UPDATE_COMMAND="composer update --no-interaction --prefer-source --verbose"
-
-composer diagnose
+INSTALL_COMMAND="composer diagnose; composer install --no-interaction --prefer-dist --verbose"
+UPDATE_COMMAND="composer diagnose; composer update --no-interaction --prefer-source --verbose"
 
 if [ "$deps" == "no" ]; then
     ${INSTALL_COMMAND}
